7392fac99ffb768307968dc65cbd8da608ac4b06
vikoiv <vikoiv256@gmail.com>
2019-01-28 21:43:42 +0200

    README,bash script updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tweepy
 python twitter bot
+
+
+wget https://raw.githubusercontent.com/vikoiv/tweepy/master/tweepy.sh
+
+bash tweepy.sh
--- tweepy.sh
+++ tweepy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo installing python3-pip
+wget https://raw.githubusercontent.com/vikoiv/tweepy/master/conf.json
+wget https://raw.githubusercontent.com/vikoiv/tweepy/master/tw.py
 
+echo installing python3-pip
 sudo apt install python3-pip
 
 echo installing tweepy 
